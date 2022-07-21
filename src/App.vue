27c0fda11d41f764d58a69f5631518933a6badd4
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-21 12:49:24 +0200

    feat: created search bar and implemented bootstrap and axios

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: function(){
+    return{
+      apiSrcUrl: 'https://api.themoviedb.org/3/search/movie?api_key=5ff24ba7734ff867e412b5136fda3d11&query='
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~bootstrap/scss/bootstrap.scss'
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Search..." aria-label="Search" aria-describedby="basic-addon1">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
